d7f18ffca6c039b2809a73dae56340cb5840ca79
abstractlyZach <zach3lee@gmail.com>
2021-04-06 23:06:37 -0700

    remove unnecessary logging

--- src/kak_plugins/os.py
+++ src/kak_plugins/os.py
@@ -3,11 +3,7 @@ from collections.abc import Callable
 
 
 def get_git_root(path_exists_function: Callable, path: str) -> str:
-    import logging
-
-    logging.info("getting git root")
     while not path_exists_function(f"{path}/.git"):
-        logging.info("loop")
         if path == "/":
             raise RuntimeError(f"{path} is not in a git directory")
         path = os.path.dirname(path)
