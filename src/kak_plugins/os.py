f3c8d76e5ec47316820b8feb9a29cb1b17745943
abstractlyZach <zach3lee@gmail.com>
2021-04-06 23:10:43 -0700

    Add docstring and rename arg

--- src/kak_plugins/os.py
+++ src/kak_plugins/os.py
@@ -2,9 +2,12 @@ import os
 from collections.abc import Callable
 
 
-def get_git_root(path_exists_function: Callable, path: str) -> str:
-    while not path_exists_function(f"{path}/.git"):
-        if path == "/":
-            raise RuntimeError(f"{path} is not in a git directory")
-        path = os.path.dirname(path)
-    return path
+def get_git_root(path_exists_function: Callable, current_path: str) -> str:
+    """Find the absolute path of the given path's git root"""
+    # Traverse up the directory tree until we find a directory
+    # that contains a .git directory
+    while not path_exists_function(f"{current_path}/.git"):
+        if current_path == "/":
+            raise RuntimeError(f"{current_path} is not in a git directory")
+        current_path = os.path.dirname(current_path)
+    return current_path
