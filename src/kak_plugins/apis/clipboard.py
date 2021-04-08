26958b3dca2853c3c7d033a98c85c9b8b53433f9
abstractlyZach <zach3lee@gmail.com>
2021-04-07 22:55:56 -0700

    Refactor and test clipboard with test doubles (#34)

--- src/kak_plugins/apis/clipboard.py
+++ src/kak_plugins/apis/clipboard.py
@@ -1,11 +1,13 @@
+import collections
 import logging
 import os
-import subprocess
+from collections.abc import Callable
 
-# TODO: how do I test this file??
+# represents a unit of work for writing to a clipboard
+ClipboardJob = collections.namedtuple("ClipboardJob", ["clipboard_command", "message"])
 
 
-def get_clipboard_command() -> str:
+def get_clipboard_command() -> str:  # pragma: nocover
     clipboard = os.getenv("CLIPBOARD")
     if clipboard is None:
         raise EnvironmentError("CLIPBOARD is not defined")
@@ -13,7 +15,14 @@ def get_clipboard_command() -> str:
         return clipboard
 
 
-def write_to_clipboard(message: str, clipboard_command: str) -> None:
-    logging.debug(f"Clipboard is {clipboard_command}")
-    subprocess.run(clipboard_command, input=message, text=True)  # noqa: S603
-    logging.debug(f"writing '{message}' to clipboard")
+def write_to_clipboard(runner: Callable, job: ClipboardJob) -> None:
+    """Make a system call to write to a clipboard.
+
+    args:
+        - runner: a callable like subprocess.run that will make a system call
+        - job: the unit of work to perform
+    """
+    logging.debug(f"writing '{job.message}' to {job.clipboard_command}")
+    runner(
+        job.clipboard_command, input=job.message, text=True, check=True
+    )  # noqa: S603
