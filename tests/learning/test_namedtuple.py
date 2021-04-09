a5c08a25b8959cef261115ffaba87b5f8ec30810
abstractlyZach <zach3lee@gmail.com>
2021-04-09 08:25:57 -0700

    Add new learning test

--- tests/learning/test_namedtuple.py
+++ tests/learning/test_namedtuple.py
@@ -1,5 +1,7 @@
 from typing import NamedTuple
 
+import pytest
+
 
 class Employee(NamedTuple):
     name: str
@@ -10,3 +12,8 @@ def test_instantiate_with_dict():
     """You can unpack a dict to insert named fields into a NamedTuple constructor"""
     bob = Employee(**{"name": "Bob", "id": 123})
     assert bob == Employee(name="Bob", id=123)
+
+
+def test_instantiate_with_dict_with_too_many_items():
+    with pytest.raises(TypeError):
+        Employee(**{"name": "Bob", "id": 123, "extra": "woops"})
