fca483a0127acd6e834ad9ca3fcc68703f99e0f0
abstractlyZach <zach3lee@gmail.com>
2021-04-08 21:25:18 -0700

    Fix kak tests and write failure test

--- tests/apis/test_kak.py
+++ tests/apis/test_kak.py
@@ -1,24 +1,36 @@
 from unittest import mock
 
+import pytest
+
 from kak_plugins import line_range
 from kak_plugins.apis import kak
 
 
-class RunnerStub(object):
-    def __init__(self, output):
-        self._output = output
-
-    def __call__(self, *args, **kwargs):
-        return RunResultStub(self._output)
-
-
-class RunResultStub(object):
+class RunSuccessStub(object):
     def __init__(self, output):
         self._output = output
 
     @property
     def stdout(self):
         return self._output
+
+    @property
+    def returncode(self):
+        return 0
+
+
+class RunFailureStub(object):
+    def __init__(self, err_message, exit_code=1):
+        self._err_message = err_message
+        self._exit_code = exit_code
+
+    @property
+    def stderr(self):
+        return self._err_message
+
+    @property
+    def returncode(self):
+        return self._exit_code
 
 
 def test_parse_single_width():
@@ -42,15 +54,29 @@ def test_parse_backwards_selection():
 def test_kcr_get_correct_call():
     runner_spy = mock.MagicMock()
     runner_spy.return_value.stdout = b"[]"
+    runner_spy.return_value.returncode = 0
     kak.kcr_get(runner_spy, ["a", "b", "c"])
     runner_spy.assert_called_once_with(
         ["kcr", "get", "--value", "a", "--value", "b", "--value", "c"],
         capture_output=True,
-        check=True,
     )
 
 
 def test_kcr_get_gets_processed_correctly():
-    runner_stub = RunnerStub(b'["buffile", "18.1,22.7"]\n')
+    success = RunSuccessStub(b'["buffile", "18.1,22.7"]\n')
+
+    def runner_stub(*args, **kwargs):
+        return success
+
     result = kak.kcr_get(runner_stub, ["a", "b", "c"])
     assert result == ["buffile", "18.1,22.7"]
+
+
+def test_kcr_get_handles_error():
+    failure = RunFailureStub(b"Something has gone wrong\n")
+
+    def runner_stub(*args, **kwargs):
+        return failure
+
+    with pytest.raises(RuntimeError):
+        kak.kcr_get(runner_stub, ["a", "b", "c"])
