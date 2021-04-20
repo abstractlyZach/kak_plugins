467701ceba2b91aa1d36be2355cf578392fa7486
abstractlyZach <zach3lee@gmail.com>
2021-04-20 09:14:35 -0700

    refactor github url conversion

--- src/kak_plugins/apis/git.py
+++ src/kak_plugins/apis/git.py
@@ -14,17 +14,26 @@ class RepoApi(object):
     def __init__(self, repo: git.Repo, configuration: Dict) -> None:
         self._repo = repo
         self._config = configuration
-        self._set_github_url()
         logging.debug(f"config is {self._config}")
+        self._set_github_url()
 
     def _set_github_url(self) -> None:
         """Convert an https or ssh remote into a github url"""
         remote_url = self._repo.remotes[REMOTE].url
         if "@" in remote_url:
             logging.debug(f"Remote URL is an ssh URL: {remote_url}")
-            remote_url = self._get_github_url(remote_url)
+            remote_url = self._get_github_base_url(remote_url)
         remote_url = remote_url.rstrip(".git")
         self._github_url = remote_url
+
+    def _get_github_base_url(self, remote_url: str) -> str:
+        """Search through the config to convert an ssh remote to a Github URL"""
+        for ssh_login, url_replacement in self._config["remotes"].items():
+            if remote_url.startswith(ssh_login):
+                logging.debug(f"Remote URL starts with ssh_login: {ssh_login}")
+                return _convert_ssh_remote_to_github_url(remote_url, url_replacement)
+        else:
+            raise RuntimeError("Could not find SSH to URL conversion in config: {}")
 
     @property
     def current_branch(self) -> str:
@@ -35,15 +44,6 @@ class RepoApi(object):
     def github_url(self) -> str:
         """Return the GitHub URL to this repo"""
         return self._github_url
-
-    def _get_github_url(self, remote_url: str) -> str:
-        # if we have an ssh remote, convert it to a URL
-        for ssh_login, url_replacement in self._config["remotes"].items():
-            if remote_url.startswith(ssh_login):
-                logging.debug(f"Remote URL starts with ssh_login: {ssh_login}")
-                return _convert_ssh_remote_to_github_url(remote_url, url_replacement)
-        else:
-            raise RuntimeError("Could not find SSH to URL conversion in config: {}")
 
     def get_permalink(
         self, path_from_git_root: str, kak_state: kak.KakouneState
@@ -56,6 +56,7 @@ class RepoApi(object):
 
 
 def _convert_ssh_remote_to_github_url(remote_url: str, github_base_url: str) -> str:
+    """Format an SSH remote url and a base github URL to get a nice github link to a repo"""
     ssh_user, repo_path = remote_url.split(":")
     if not github_base_url.endswith("/"):
         github_base_url += "/"
